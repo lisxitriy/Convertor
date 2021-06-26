6ab58c341bc285d26801d6c82a85864337ddcde3
author <email>
2021-06-26 15:29:20 +0300

    Initial Commit

---
+++ Convertor/ConvertorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertorApp.swift
+//  Convertor
+//
+//  Created by Olga Trofimova on 26.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct ConvertorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
